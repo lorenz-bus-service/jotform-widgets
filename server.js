6581a8dc460426a3592c80c4b6442550d2edacbd
Craig Buchanan <craig.buchanan@cogniza.com>
2023-02-16 10:07:19 -0600

    updating comments using http-proxy-middleware fixing /api/bamboohr route

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 /*
-## Installation
-npm install --save express express-request-proxy
 
-## Reference
-- https://github.com/dvonlehman/express-request-proxy
+## Installation
+npm install --save express http-proxy-middleware morgan
+
+## Dependencies
+- https://www.npmjs.com/package/express
+- https://www.npmjs.com/package/http-proxy-middleware
+- https://www.npmjs.com/package/morgan
+
 */
 
 const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-let requestProxy = require("express-request-proxy");
+const PORT = 3000
+const HOST = 'localhost'
 
 const app = express();
 app.use(express.json());
+// logging
+app.use(morgan('dev'));
 
 app.get('/widgets/find_employee', (req, res) => {
     res.sendFile(`${__dirname}/widgets/com.bamboohr.find_employee.html`)
@@ -21,18 +30,20 @@ app.get('/api', (req, res) => {
     res.send('API root');
 });
 
-app.get("/api/employees", 
-    requestProxy({
-        // cache: redis.createClient(),
-        // cacheMaxAge: 60,
-        url: `https://api.bamboohr.com/api/gateway.php/${process.env.BAMBOOHR_API_SUBDOMAIN}/v1/employees/directory`,
+app.use("/api/bamboohr", 
+    createProxyMiddleware({
+        target: `https://api.bamboohr.com/api/gateway.php/${process.env.BAMBOOHR_API_SUBDOMAIN}/v1`,
+        changeOrigin: true,
         headers: {
             Accept: 'application/json',
             Authorization: "Basic " + Buffer.from(process.env.BAMBOOHR_API_KEY + ":password").toString('base64')
-        }
+        },
+        pathRewrite: {
+            [`^/api/bamboohr`]: '',
+        },    
     })
 );
 
-app.listen(3000, 'localhost', () => {
-  console.log('Service running at http://localhost:3000');
+app.listen(PORT, HOST, () => {
+    console.log(`Service running at http://${HOST}:${PORT}`);
 });
